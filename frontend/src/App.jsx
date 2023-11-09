cbf2ae504b3c3ade40b830736544043327a7cb7d
esimo13 <obak139@gmail.com>
2023-11-09 19:52:00 +0600

    view auctions frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 // src/App.js
 
 import React from 'react';
+import AuctionList from './components/AuctionList';
 import CreateAuctionForm from './components/CreateAuctionForm';
+import Auctions from './components/AuctionList';
 
 const App = () => {
   return (
     <div>
       <h1>Auction App</h1>
-      <CreateAuctionForm />
+      {/* <CreateAuctionForm /> */}
+      {/* <Auctions /> */}
+      <AuctionList />
     </div>
   );
 };
---
+++ frontend/src/components/AuctionList.jsx
@@ -0,0 +1,38 @@
+// src/components/AuctionList.js
+
+import React, { useState, useEffect } from 'react';
+
+const AuctionList = () => {
+  const [auctions, setAuctions] = useState([]);
+
+  useEffect(() => {
+    // Fetch all auctions from your backend
+    const fetchAuctions = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/auctions');
+        const data = await response.json();
+
+        setAuctions(data.auctions);
+      } catch (error) {
+        console.error('Error fetching auctions:', error);
+      }
+    };
+
+    fetchAuctions();
+  }, []); // The empty dependency array ensures the effect runs only once on component mount
+
+  return (
+    <div>
+      <h2>Auction List</h2>
+      <ul>
+        {auctions.map((auction) => (
+          <li key={auction._id}>
+            <strong>{auction.title}</strong> - {auction.description}, Starting Bid: ${auction.startingBid}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AuctionList;
