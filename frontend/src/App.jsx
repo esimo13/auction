fe1c1dce77b052b4d7bff96bd3a2c6fca3dee7d0
esimo13 <obak139@gmail.com>
2023-11-09 20:08:58 +0600

    install rrd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,12 @@
 import React from 'react';
 import AuctionList from './components/AuctionList';
 import CreateAuctionForm from './components/CreateAuctionForm';
-import Auctions from './components/AuctionList';
 
 const App = () => {
   return (
     <div>
       <h1>Auction App</h1>
-      {/* <CreateAuctionForm /> */}
-      {/* <Auctions /> */}
+      <CreateAuctionForm />
       <AuctionList />
     </div>
   );
